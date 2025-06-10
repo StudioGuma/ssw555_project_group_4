2a5bee3a93cd53a3dd28dd31c235623729830380
StudioGuma <purpleeyedguma@aol.com>
2025-06-09 21:49:14 -0400

    modified program to store table info

--- ssw555_proj.py
+++ ssw555_proj.py
@@ -24,53 +24,102 @@ def main() -> int:
 			raise Exception(argv[0] + ": input file is not a GEDCOM file (.ged)")
 
 		indi_table = PrettyTable()
-		indi_table.field_names = ["ID", "Name", "Gender", "Date of Birth", "Age", "Alive?",
-		"Date of Death", "Child(ren)", "Spouse(s)"]
+		indi_table.field_names = ["ID", "Name", "Gender", "Date of Birth", "Date of Death",
+		"Child in", "Spouse in"]
 		fam_table = PrettyTable()
-		fam_table.field_names = ["ID", "Date of Marriage", "Divorced?", "Spouse ID(s)",
-		"Spouse Name(s)", "Child(ren)"]
+		fam_table.field_names = ["ID", "Date of Marriage", "Husband", "Wife", "Children", "Date of Divorce"]
 
 
 		with open(argv[1], "r") as ged:
 			line: string = ged.readline()
-			current_row: list = []
+
+			cur_indi_row: list = ["N/A", "N/A", "N/A", "N/A", "N/A", [], []]
+			cur_fam_row: list = ["N/A", "N/A", "N/A", "N/A", [], "N/A"]
+
+			is_birth: bool = False
+			is_death: bool = False
+			is_marr: bool = False
+			is_div: bool = False
 
 			while (line != ""):
-				print("--> " + line, end="")
-
 				params: list = line.split(" ", 2)
 				for i in range(len(params)):
 					params[i] = params[i].rstrip()
 
-				if (len(params) > 2 and is_valid_tag(int(params[0]), params[2])):
-					if (params[2] == "INDI"):
-						print("TODO")
+				level: int = int(params[0])
+				match level:
+					case 0:
+						# add current row to the table
+						if (len(params) > 2 and is_valid_tag(level, params[2])):
+							match params[2]:
+								case "INDI":
+									if (cur_indi_row[0] != "N/A"):
+										indi_table.add_row(cur_indi_row)
+									cur_indi_row = ["N/A", "N/A", "N/A", "N/A", "N/A", [], []]
 
-					elif (params[2] == "FAM"):
-						print("TODO")
+									cur_indi_row[0] = params[1]
 
-				elif (is_valid_tag(int(params[0]), params[1])):
-					print("TODO")
+								case "FAM":
+									if (cur_fam_row[0] != "N/A"):
+										fam_table.add_row(cur_fam_row)
+									cur_fam_row = ["N/A", "N/A", "N/A", "N/A", [], "N/A"]
 
-				# if (len(params) > 2 and (params[2] == "INDI" or params[2] == "FAM")):
-				# 	print("<-- " + params[0] + " | " + params[2] + " | ", end="")
-				# 	if (is_valid_tag(int(params[0]), params[2])):
-				# 		print("Y", end="")
-				# 	else:
-				# 		print("N", end="")
-				# 	print(" | " + params[1])
+									cur_fam_row[0] = params[1]
 
-				# else:
-				# 	print("<-- " + params[0] + " | " + params[1] + " | ", end="")
-				# 	if (is_valid_tag(int(params[0]), params[1])):
-				# 		print("Y", end="")
-				# 	else:
-				# 		print("N", end="")
-					
-				# 	if (len(params) > 2):
-				# 		print(" | " + params[2])
-				# 	else:
-				# 		print("")
+					case 1:
+						if (is_valid_tag(level, params[1])):
+							match params[1]:
+								case "NAME":
+									cur_indi_row[1] = params[2]
+
+								case "SEX":
+									cur_indi_row[2] = params[2]
+
+								case "BIRT":
+									is_birth = True
+
+								case "DEAT":
+									is_death = True
+
+								case "FAMC":
+									cur_indi_row[5].append(params[2])
+
+								case "FAMS":
+									cur_indi_row[6].append(params[2])
+
+								case "MARR":
+									is_marr = True
+
+								case "HUSB":
+									cur_fam_row[2] = params[2]
+
+								case "WIFE":
+									cur_fam_row[3] = params[2]
+
+								case "CHIL":
+									cur_fam_row[4].append(params[2])
+
+								case "DIV":
+									is_div = True
+
+					case 2:
+						if (len(params) > 2 and is_valid_tag(level, params[1])):
+							# guaranteed to be DATE
+							if is_birth:
+								cur_indi_row[3] = params[2]
+								is_birth = False
+
+							elif is_death:
+								cur_indi_row[4] = params[2]
+								is_death = False
+
+							elif is_marr:
+								cur_fam_row[1] = params[2]
+								is_marr = False
+
+							elif is_div:
+								cur_fam_row[5] = params[2]
+								is_div = False
 
 				line = ged.readline()
 		
