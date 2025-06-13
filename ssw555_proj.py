295078203e46ebd6d5ec1f1e03abdae5b72d4646
StudioGuma <purpleeyedguma@aol.com>
2025-06-12 22:43:59 -0400

    changed to built-in boolean False

--- ssw555_proj.py
+++ ssw555_proj.py
@@ -14,7 +14,7 @@ def is_valid_tag(level: int, tag: str) -> bool:
 		case 2:
 			return (tag == "DATE")
 
-	return false
+	return False
 
 def is_leap_year(year: int) -> bool:
 	return (year % 400 == 0) or (year % 4 == 0 and year % 100 > 0)
