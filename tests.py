3e1f925415b930eaaf0255236f9ad22f6bc61b55
StudioGuma <purpleeyedguma@aol.com>
2025-06-16 18:10:42 -0400

    tests file is now tracked

---
+++ tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import unittest
+from ssw555_proj import is_valid_date_str
+
+class Tests(unittest.TestCase):
+	def test_valid_date_positives(self):
+		self.assertTrue(is_valid_date_str("13 JUN 2025"))
+		self.assertTrue(is_valid_date_str("25 DEC 1997"))
+		self.assertTrue(is_valid_date_str("1 JAN 1970"))
+		self.assertTrue(is_valid_date_str("15 FEB 1870"))
+		self.assertTrue(is_valid_date_str("29 FEB 2024"))
+
+	def test_valid_date_negatives(self):
+		self.assertFalse(is_valid_date_str("13 JUNE 2025"))
+		self.assertFalse(is_valid_date_str("01 JAN 1970"))
+		self.assertFalse(is_valid_date_str("19 8 4"))
+		self.assertFalse(is_valid_date_str("3 APR 3"))
+		self.assertFalse(is_valid_date_str("thisisavaliddatestringiswear"))
+		self.assertFalse(is_valid_date_str(""))
+		self.assertFalse(is_valid_date_str("29 FEB 2023"))
+		self.assertFalse(is_valid_date_str("31 APR 2100"))
+
+if __name__ == "__main__":
+	unittest.main(verbosity=2)
